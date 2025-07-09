744478aec37d1c6e4c5f1f62af2a4ec66614ab5e
Jaepy99 <ljp990327@gmail.com>
2025-07-09 23:35:14 +0900

    change prep code

--- EDA.R
+++ EDA.R
@@ -1,4 +1,10 @@
 ##### Exploratory Data Analysis #####
+load("25-spring.Rdata")
+
+grade.lms <- dt.prep[[1]]
+attendance <- dt.prep[[2]]
+aleks <- dt.prep[[3]]
+
 library(ggplot2)
 ### grade
 # A tibble: 6 × 2
@@ -10,20 +16,21 @@ library(ggplot2)
 # 4 B0        9
 # 5 C+        7
 
-grade.prep %>% 
+grade.lms %>% 
   ggplot(aes(x = 성적, y = aleks)) +
   geom_boxplot()
 
-grade.prep %>% 
-  ggplot(aes(x = 성적, y = 조별문제)) +
+grade.lms %>%
+  ggplot(aes(x = 성적, y = 숙제합계)) +
   geom_boxplot()
 
-grade.prep %>% 
-  ggplot(aes(x = test, y = score)) +
+grade.lms %>% 
+  filter(week == '중간1' | week == '중간2' | week == '중간3' | week == '기말') %>% 
+  ggplot(aes(x = week, y = score)) +
   geom_boxplot()
 
-grade.prep %>% 
-  ggplot(aes(x = test, y = score)) +
+grade.lms %>%
+  filter(week == '중간1' | week == '중간2' | week == '중간3' | week == '기말') %>% 
+  ggplot(aes(x = week, y = score)) +
   geom_boxplot() +
   facet_wrap(~성적)
-

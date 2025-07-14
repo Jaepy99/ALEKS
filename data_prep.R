b78ddee464f3b536c2850649148dee84c2d5a4b3
Jaepy99 <ljp990327@gmail.com>
2025-07-14 13:23:52 +0900

    change prep code

--- data_prep.R
+++ data_prep.R
@@ -1,13 +1,13 @@
 # 25-1 통계학 수업의 성적 및 ALEKS 사용간 상관관계에 대해서
 
 ##### Data 설명 #####
-# 성적 반영비율 
+# 성적 반영비율
 # 출석:ALEKS:조별과제:중간1:중간2:중간3:기말
 #   5:    10:       5:   20:   20:   20:  20
 
 # 출석
 # A:출석(Attendance), T:지각(Tardy), B:결석(Absence), E:조퇴(Early leave), U:미확인(Unidentified)
-# 공휴일: 2025-03-03삼일절 대체 휴일, 2025-05-05어린이날, 석가탄신일, 
+# 공휴일: 2025-03-03삼일절 대체 휴일, 2025-05-05어린이날, 석가탄신일,
 #         2025-05-06대체 휴일,	2025-06-03제21대 대통령선거, 2025-06-06현충일
 
 # ALEKS 모듈
@@ -44,25 +44,25 @@ aleks <- dt[[4]]
 
 ##### data prep #####
 ### grade and lms
-grade.lms <- grade %>% 
-  select(-중간1 & -중간2 & -중간3 & -기말 & -조별문제) %>% 
-  left_join(lms, by = 'id') %>% 
-  filter(id != 99) %>% 
-  select(-week_12) %>% 
+grade.lms <- grade %>%
+  select(-중간1 & -중간2 & -중간3 & -기말 & -조별문제) %>%
+  left_join(lms, by = 'id') %>%
+  filter(id != 99) %>%
+  select(-"12") %>%
   pivot_longer(cols = -c(id, 숙제합계, 성적, 평점, 출석, aleks, total),
-               names_to = "week", values_to = "score") %>% 
-  mutate(week = factor(week, levels = c("week_2", "week_3", "week_5","중간1", 
-                                        "week_6", "week_7", "중간2", "week_9", 
-                                        "week_10", "week_11", "중간3", "week_13", 
-                                        "week_14", "week_15", "기말")))
+               names_to = "week", values_to = "score") %>%
+  mutate(week = factor(week, levels = c("2", "3", "중간1", "5",
+                                        "6", "7", "중간2", "9",
+                                        "10", "11", "중간3", "13",
+                                        "14", "15", "기말")))
 
 ### aleks
-aleks.prep <- aleks %>% 
+aleks.prep <- aleks %>%
   pivot_longer(cols = -id, names_to = "module",
-               values_to = "master") %>% 
-  mutate(module = factor(module, levels = c("Module_1", "Module_2", "Module_3", 
-                                            "Module_4", "Module_5", "Module_6", 
-                                            "Module_7", "Module_8", "Module_9", 
+               values_to = "master") %>%
+  mutate(module = factor(module, levels = c("Module_1", "Module_2", "Module_3",
+                                            "Module_4", "Module_5", "Module_6",
+                                            "Module_7", "Module_8", "Module_9",
                                             "Module_10", "Module_11", "Module_12",
                                             "Module_13", "Total_Grade")))
 
